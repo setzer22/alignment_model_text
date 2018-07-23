4ab5dfc5e6df5df9ce90bf91c36567005c9c28e1
Josep <jsanchezfsms@gmail.com>
2018-07-23 16:58:48 +0200

    Code cleanup

--- install-local-jars.sh
+++ install-local-jars.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mvn install:install-file -Dfile=./local-jars/jbpt-0.2.393.jar -DgroupId=org.jbpt -DartifactId=jbpt -Dversion=0.2.393 -Dpackaging=jar -DgeneratePom=true
-mvn install:install-file -Dfile=./local-jars/nlp4bpm_commons-0.1.0.jar -DgroupId=edu.upc -DartifactId=nlp4bpm_commons -Dversion=0.1.0 -Dpackaging=jar -DgeneratePom=true
 mvn install:install-file -Dfile=./local-jars/OpenXES-20160212.jar -DgroupId=org.deckfour -DartifactId=xes -Dversion=0.1.0 -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=./local-jars/Jfreeling.jar -DgroupId=edu.upc -DartifactId=jfreeling -Dversion=4.1.0 -Dpackaging=jar -DgeneratePom=true
